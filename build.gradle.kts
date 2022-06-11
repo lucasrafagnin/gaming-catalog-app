a631f8407f9c05d7792b4172bed42a7861f6f2b5
lucasrafagnin <lucasrafagnin7@gmail.com>
2022-06-11 14:43:07 -0300

    Migrate build.gradle (root level) and settings.gradle to Kotlin DSL

---
+++ build.gradle.kts
@@ -0,0 +1,23 @@
+buildscript {
+    val kotlinVersion by extra("1.5.31")
+
+    repositories {
+        google()
+        mavenCentral()
+    }
+
+    dependencies {
+        classpath("com.android.tools.build:gradle:7.1.3")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.4.2")
+        classpath("com.google.dagger:hilt-android-gradle-plugin:2.42")
+    }
+}
+
+allprojects {
+
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,2 @@
+rootProject.name = "GamingCatalog"
+include(":app")
